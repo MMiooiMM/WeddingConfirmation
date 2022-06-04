73e875dc62ce73c5afa1f4ed516965c78b8ae59b
Mio <mio093611@gmail.com>
2022-06-04 19:52:36 +0800

    (feat) 加入流程控制

---
+++ WeddingConfirmation/Constraints/Joining.cs
@@ -0,0 +1,8 @@
+﻿namespace WeddingConfirmation.Constraints
+{
+    public static class Joining
+    {
+        public const string Yes = "Yes";
+        public const string No = "No";
+    }
+}
---
+++ WeddingConfirmation/Constraints/Send.cs
@@ -0,0 +1,10 @@
+﻿namespace WeddingConfirmation.Constraints
+{
+    public static class Send
+    {
+        public const string Paper = "Paper";
+        public const string Email = "Email";
+        public const string None = "None";
+
+    }
+}
