f238dce3d7499528c15a7661e488fa68afe86248
Mio <mio093611@gmail.com>
2022-06-12 23:47:36 +0800

    (feat) 增加地點路由

--- WeddingConfirmation/Constraints/Location.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WeddingConfirmation.Constraints
-{
-    public class Venue
-    {
-        public static string Yilan = "宜蘭";
-        public static string Taipei = "台北";
-    }
-}
---
+++ WeddingConfirmation/Constraints/Venue.cs
@@ -0,0 +1,8 @@
+﻿namespace WeddingConfirmation.Constraints
+{
+    public class Venue
+    {
+        public const string Yilan = "Yilan";
+        public const string Taipei = "Taipei";
+    }
+}
